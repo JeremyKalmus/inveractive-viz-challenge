648aac2655e3c28dfe0ed90ff1f051d0cc82ba4d
JeremyKalmus <Jeremykalmus@gmail.com>
2021-01-31 08:50:08 -0800

    dashboard working

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,13 @@
 
-function init() {
+function init(sample_values, otu_ids, otu_labels, meta_data) {
 
-d3.json('samples.json').then(sampledata => {
-    console.log(sampledata);
-
-
-    //create variables for samples
-    //Sample Values
-    var sample_values = sampledata.samples[0].sample_values;
-    console.log(sample_values);
-    //Ids
-    var otu_ids = sampledata.samples[0].otu_ids;
-    console.log(otu_ids);
-    // labels
-    var otu_labels = sampledata.samples[0].otu_labels;
-    console.log(otu_labels);
-
-    
-
-
-    //Horizontal Bar Chart
+    var barChart = function(sample_values, otu_ids, otu_labels) {
+        //Horizontal Bar Chart
 
     var bar_ids = otu_ids.slice(0,10).map(d => 'OTU '+ d).reverse();
     console.log("Top 10 IDs: "+ bar_ids);
 
-    var bar_labels = sampledata.samples[0].otu_labels.slice(0,10).reverse();
+    var bar_labels = otu_labels.slice(0,10).reverse();
     console.log("labels:" + bar_labels);
 
     //sort and slice for top 10
@@ -55,9 +38,10 @@ d3.json('samples.json').then(sampledata => {
     };
     
     Plotly.newPlot('bar', data, layout);
+};
+barChart(sample_values, otu_ids, otu_labels);
 
-    
-    
+var bubbleChart = function(sample_values, otu_ids, otu_labels){
     //buble Chart
     var trace2 = {
         x: otu_ids,
@@ -86,32 +70,106 @@ d3.json('samples.json').then(sampledata => {
         showlegend: false,
     }
 
-    Plotly.newPlot('bubble', data2, layout2)
+    Plotly.newPlot('bubble', data2, layout2)}
+
+    bubbleChart(sample_values, otu_ids, otu_labels);
 
     //display metadata
-
-    var meta_data = d3.map(sampledata.metadata[0]).entries()
-    console.log(meta_data)
-
-    var ul_meta = d3.select("#sample-metadata").append("ul").attr("id", "ul-meta-data");
-    console.log(sampledata.metadata[0]);
+        var meta_data = d3.map(meta_data).entries();
+        console.log(meta_data);
+        
+        
+        var ul_meta = d3.select("#sample-metadata").attr("id", "ul-meta-data");
     
-    d3.select("#ul-meta-data")
-        .selectAll("li")
-        .data(meta_data)
-        .enter()
-        .append("li")
-        .text(function(d) {
-            return `${d.key} : ${d.value}`
+        d3.select("#ul-meta-data")
+            .selectAll("p")
+            .data(meta_data)
+            .enter()
+            .append("p")
+            .text(function(d) {
+                return `${d.key} : ${d.value}`
+            });
+
+    };
+
     
+
+
+    
+
+    
+    
+
+
+
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+var optionChanged = function(select) {
+    console.log(select);
+
+        d3.json('samples.json').then(function(sampledata){
+            console.log(sampledata.samples);
+
+        var new_sample = sampledata.samples.filter(function(samples) {
+            console.log(samples);
+            return samples.id === select;
+        })
+        console.log(new_sample);
+
+
+        new_meta = sampledata.metadata.filter(function(meta_data){
+            return meta_data.id == select;
         });
+        console.log(new_meta);
 
 
+        sample_values = new_sample[0].sample_values;
+        otu_ids = new_sample[0].otu_ids;
+        otu_labels = new_sample[0].otu_labels;
+        meta_data = new_meta;
+
+        init(sample_values, otu_ids, otu_labels, meta_data);
+
+
+    })};
+
+
+
+d3.json('samples.json').then(sampledata => {
+    console.log(sampledata);
+ //Sample Values for first
+ var sample_values = sampledata.samples[0].sample_values;
+ console.log(sample_values);
+ //Ids
+ var otu_ids = sampledata.samples[0].otu_ids;
+ console.log(otu_ids);
+ // labels
+ var otu_labels = sampledata.samples[0].otu_labels;
+ console.log(otu_labels);
+ var meta_data = sampledata.metadata[0];
+ console.log(meta_data);
+
+ 
+    //populate dropdown with sample values: 
+    var datasets = sampledata["names"];
+    console.log(datasets);
+    
+    var drowndown_menu = function(names){
+        
+        var selectTag = d3.select("#selDataset")
+        var menu_items = selectTag.selectAll("option").data(names);
+
+        menu_items.enter().append('option')
+        .attr('value', function(d){
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        });
+    }
+
+    drowndown_menu(sampledata.names);
+
+    init(sample_values, otu_ids, otu_labels, meta_data);
 
 });
-
-
-
-}
-
-init()
